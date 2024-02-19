2992ff992a6f4d9af3771805db8763a86016a5a4
FerMelero <160114946+FerMelero@users.noreply.github.com>
2024-02-19 19:15:33 +0100

    script_carpeta_archivos.sh

--- script_carpeta_archivos.sh
+++ script_carpeta_archivos.sh
@@ -9,8 +9,44 @@ echo "Creando archivos .txt en la carpeta..."
 touch "$1/$2"
 touch "$1/$3"
 
-# Ejecutar el programa de Python
-echo "Ejecutando el programa de Python..."
-python3 archivo_numeros.py
+echo "Proceso 1 completado."
+
+# Ciclo principal del menú
+while true; do
+    echo "Menú:"
+    echo "1. Crear carpeta nueva"
+    echo "2. Crear archivos"
+    echo "3. Ejecutar programa en Python"
+    echo "4. Salir"
+
+    read -p "Ingrese su elección: " opcion
+
+    case $opcion in
+        1)
+            read -p "Ingrese el nombre de la carpeta nueva: " carpeta
+            mkdir "$carpeta"
+            ;;
+        2)
+            read -p "Ingrese el nombre del primer archivo: " archivo1
+            read -p "Ingrese el nombre del segundo archivo: " archivo2
+            touch "$archivo1.txt"
+            touch "$archivo2.txt"
+            ;;
+        3)
+            echo "Ejecutando el programa de Python..."
+            python3 archivo_numeros.py
+            ;;
+        4)
+            echo "Saliendo del menú..."
+            break
+            ;;
+        *)
+            echo "Opción no válida"
+            ;;
+#Fin del case
+    esac
+
+# Fin del while
+done
 
 echo "Proceso completado."
